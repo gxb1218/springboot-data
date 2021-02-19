f8273f2c341aae5733925e59898b0077efbf1ea2
guoxb <guoxb@huatech.com>
2021-02-19 13:59:37 +0800

    springBootdata

---
+++ src/test/java/com/gxb/SpringbootDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
