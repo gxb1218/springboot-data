7c0005fe62bb55d5f342a96ebfcfa09f6d3c5ae5
guoxb <guoxb@huatech.com>
2021-02-20 09:10:07 +0800

    昨天修改的

---
+++ src/main/java/com/gxb/controller/JDBCController.java
@@ -0,0 +1,16 @@
+/**
+ * @Description: TODO
+ * @author: scott
+ * @date: 2021年02月19日 15:04
+ */
+package com.gxb.controller;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JDBCController {
+
+    JdbcTemplate  JdbcTemplate;
+
+}
--- src/test/java/com/gxb/SpringbootDataApplicationTests.java
+++ src/test/java/com/gxb/SpringbootDataApplicationTests.java
@@ -1,13 +1,27 @@
 package com.gxb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import javax.xml.transform.Source;
+import java.sql.Connection;
 
 @SpringBootTest
 class SpringbootDataApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception{
+        System.err.println(dataSource.getClass());
+        //创建数据库连接
+        Connection connection =dataSource.getConnection();
+        System.err.println("connection="+connection);
+        //关闭
+        connection.close();
     }
 
 }
